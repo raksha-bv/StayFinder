27ee91c80a93b9798c5f4fe001f813007124d76f
raksha-bv <rakshabv04@gmail.com>
2025-06-19 12:12:33 +0530

    fix: update frontend url in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: "50mb" })); // Increased limit for image uploads
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://stayfinder-phi.vercel.app/",
     credentials: true,
   })
 );
